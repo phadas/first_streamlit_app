b1c3a97cb52d26199cf898de82ce265258cd01b8
phadas <146656415+phadas@users.noreply.github.com>
2023-10-01 18:15:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlite.header('Breakfast Menu')
+streamlite.text('Omega 3 & Bluberry Oatmeal')
+streamlite.text ('Kale, Spinach & Rocket Smothies')
+streamlite.text ('HArd-Boiled Free-Range Egg')

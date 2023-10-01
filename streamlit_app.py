a61ca766395adc3649bb350d0c46b03b25ef1128
phadas <146656415+phadas@users.noreply.github.com>
2023-10-01 18:19:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Breakfast Favoritues')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluberry Oatmeal')
-streamlit.text ('Kale, Spinach & Rocket Smothies')
-streamlit.text ('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
+streamlit.text ('🥗 Kale, Spinach & Rocket Smothies')
+streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text ('🥑🍞 Avocado Toast')
